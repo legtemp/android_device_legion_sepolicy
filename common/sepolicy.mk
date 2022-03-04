8a58c0adb92d74357ccc298362bddc39ed066534
rajkale99 <kaleraj.rk@gmail.com>
2022-03-04 13:12:31 +0530

    sepolicy:Rebrand to legion
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Legion
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,17 +10,17 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/legion/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/legion/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/legion/sepolicy/common/dynamic \
+    device/legion/sepolicy/common/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/legion/sepolicy/common/dynamic \
+    device/legion/sepolicy/common/vendor
 endif
